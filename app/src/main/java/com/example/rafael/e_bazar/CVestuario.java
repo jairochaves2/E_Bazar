65ddebfadf599205ddb01a42a1a8707785290b7b
rafael <rafaeljosev10@gmail.com>
2016-05-28 02:31:36 -0300

    Criação de toda as activity, novo icone, nova logo

---
+++ app/src/main/java/com/example/rafael/e_bazar/CVestuario.java
@@ -0,0 +1,13 @@
+package com.example.rafael.e_bazar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CVestuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cvestuario);
+    }
+}
---
+++ app/src/main/java/com/example/rafael/e_bazar/Configuracoes.java
@@ -0,0 +1,15 @@
+package com.example.rafael.e_bazar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by rafael on 27/05/16.
+ */
+public class Configuracoes extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Configuracoes);
+    }
+}
